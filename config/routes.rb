05400a811d54188e0d624cce0d29db980b71d83e
satyasinha94 <satyasinha94@gmail.com>
2019-04-12 18:49:44 -0400

    fixed remove_appointment bug caused by route overwriting params[:id] in find helper method

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :schedules, only: [:index, :create, :show, :destroy]
       post '/schedules/:id/appointments', to: 'appointments#create'
-      delete '/schedules/:id/appointments/:id', to: 'schedules#remove_appointment'
+      delete '/schedules/:id/appointments/:appt_id', to: 'schedules#remove_appointment'
     end
   end
 end
